836e23ba33de133680d73e1e8b86c967cf84c92b
Dhanesh-iitm <24ds2000112@ds.study.iitm.ac.in>
2025-08-16 17:27:04 +0530

    Update runner.py

--- executor/runner.py
+++ executor/runner.py
@@ -2,14 +2,15 @@ import pandas as pd
 import requests
 
 def execute_code(code_str):
-   shared_scope = {
-       "__builtins__": __builtins__,
-       "pd": pd,
-       "requests": requests
-   }
-   local_vars = {}
-   try:
-       exec(code_str, shared_scope, shared_scope)
-       return shared_scope.get("result", "No variable named 'result' was found.")
-   except Exception as e:
-       return f"Error during execution: {e}"
+    shared_scope = {
+        "__builtins__": __builtins__,
+        "pd": pd,
+        "requests": requests
+    }
+    try:
+        exec(code_str, shared_scope, shared_scope)
+        return {
+            "df": shared_scope.get("df")
+        }
+    except Exception as e:
+        return {"error": f"Error during execution: {e}"}
